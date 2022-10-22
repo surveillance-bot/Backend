ba0ece3f94cf5c755c5401c20010643f20ee6e7f
Piyush <piyushb544@gmail.com>
2022-10-22 13:28:37 +0530

    opening static files

--- Django/faces/views.py
+++ Django/faces/views.py
@@ -3,50 +3,61 @@ from django.contrib.staticfiles.storage import staticfiles_storage
 import os
 import cv2
 import numpy as np
+
+from surveillanceBot.settings import BASE_DIR
  # Create your views here.
 
 def data_generator(self):
-    prototxt_path = staticfiles_storage.url('modeldata/deploy.prototxt')
-    caffemodel_path = staticfiles_storage.url('modeldata/weights.caffemodel')
+    # print(BASE_DIR)
+    # url = staticfiles_storage.url('model_data/deploy.prototxt')
+    # staticfiles_storage.open('model_data/deploy.prototxt')
+    prototxt_path = staticfiles_storage.open('modeldata/deploy.prototxt')
+    caffemodel_path = staticfiles_storage.open('modeldata/weights.caffemodel')
+    # print(prototxt_path)
+    # print(caffemodel_path)
     # base_dir = os.path.dirname(__file__)
-    # prototxt_path = os.path.join(base_dir + '\model_data\deploy.prototxt')
-    # caffemodel_path = os.path.join(base_dir + '\model_data\weights.caffemodel')
-
-    # Read the model
+    # prototxt_path = os.path.join(BASE_DIR + '/'+'faces' + '/' + 'model_data' + '/' + 'deploy.prototxt')
+    # caffemodel_path = os.path.join(BASE_DIR + '/'+'faces' + '/' + 'model_data' + '/' + 'weights.caffemodel')
+    # prototxt_path = os.path.join(BASE_DIR , "/faces/model_data/deploy.prototxt")
+    # caffemodel_path = os.path.join(BASE_DIR, "/faces/model_data/weights.caffemodel")
+    
+    # print(prototxt_path,"hello")
+    # print(caffemodel_path,"hello")
+    # # Read the model
     model = cv2.dnn.readNetFromCaffe(prototxt_path, caffemodel_path)
 
-    # Create directory 'updated_images' if it does not exist
-    if not os.path.exists('updated_images'):
-        print("New directory created")
-        os.makedirs('updated_images')
+    # # Create directory 'updated_images' if it does not exist
+    # if not os.path.exists('updated_images'):
+    #     print("New directory created")
+    #     os.makedirs('updated_images')
 
-    # Loop through all images and save images with marked faces
-    for file in os.listdir(staticfiles_storage.url):
-        file_name, file_extension = os.path.splitext(file)
-        if (file_extension in ['.png','.jpg']):
-            print("Image path: {}".format(staticfiles_storage.url('images/') + file))
+    # # Loop through all images and save images with marked faces
+    # for file in os.listdir(staticfiles_storage.url):
+    #     file_name, file_extension = os.path.splitext(file)
+    #     if (file_extension in ['.png','.jpg']):
+    #         print("Image path: {}".format(staticfiles_storage.url('images/') + file))
 
-            image = cv2.imread(staticfiles_storage.url('images/') + file)
-            # print(image)
-            (h, w) = image.shape[:2]
-            blob = cv2.dnn.blobFromImage(cv2.resize(image, (300, 300)), 1.0, (300, 300), (104.0, 177.0, 123.0))
+    #         image = cv2.imread(staticfiles_storage.url('images/') + file)
+    #         # print(image)
+    #         (h, w) = image.shape[:2]
+    #         blob = cv2.dnn.blobFromImage(cv2.resize(image, (300, 300)), 1.0, (300, 300), (104.0, 177.0, 123.0))
 
-            model.setInput(blob)
-            detections = model.forward()
+    #         model.setInput(blob)
+    #         detections = model.forward()
 
-            # Create frame around face
-            for i in range(0, detections.shape[2]):
-                box = detections[0, 0, i, 3:7] * np.array([w, h, w, h])
-                (startX, startY, endX, endY) = box.astype("int")
+    #         # Create frame around face
+    #         for i in range(0, detections.shape[2]):
+    #             box = detections[0, 0, i, 3:7] * np.array([w, h, w, h])
+    #             (startX, startY, endX, endY) = box.astype("int")
 
-                confidence = detections[0, 0, i, 2]
+    #             confidence = detections[0, 0, i, 2]
 
-                # If confidence > 0.5, show box around face
-                if (confidence > 0.5):
-                    cv2.rectangle(image, (startX, startY), (endX, endY), (255, 255, 255), 2)
+    #             # If confidence > 0.5, show box around face
+    #             if (confidence > 0.5):
+    #                 cv2.rectangle(image, (startX, startY), (endX, endY), (255, 255, 255), 2)
 
-            # cv2.imwrite(base_dir + '/updated_images/' + file, image)
-            print("Image " + file + " converted successfully")
+    #         # cv2.imwrite(base_dir + '/updated_images/' + file, image)
+    #         print("Image " + file + " converted successfully")
 
 
 def face_extractor(self):
@@ -93,6 +104,6 @@ def face_extractor(self):
 
 def home(request):
     url = staticfiles_storage.url('images/faces.jpg')
-    print(url)
-    # data_generator(url)
+    # print(url)
+    data_generator(url)
     return render(request, "home.html")
